d69e803cf3d29c6dd554cbb7e84c241f61ceabd5
Jordan Hand <jorhand@microsoft.com>
2019-11-19 14:35:44 -0800

    Add config support to go port

--- go/main.go
+++ go/main.go
@@ -1,16 +1,36 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"os"
 
 	"github.com/fsnotify/fsnotify"
 )
 
+func read_config(config_name string) map[string]string {
+	f, err := os.Open("/home/jordan/.quicksync.conf")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer f.Close()
+
+	bytes, _ := ioutil.ReadAll(f)
+
+	var conf map[string]map[string]string
+	json.Unmarshal(bytes, &conf)
+	return conf[config_name]
+}
+
 func main() {
+	var config = read_config("linux_acc_vm")
+
 	fw := new(FileWatcher)
 	fw.Init()
-	fw.AddRecursive(".")
+	fw.AddRecursive(config["client_oedir"])
 	fw.Start(func(e fsnotify.Event) {
-		fmt.Println(e.Name)
+		copy_file(e.Name, e.Name)
 	})
 }
---
+++ go/scp.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func copy_file(src string, dst string) {
+	fmt.Printf("Copying %s to %s\n", src, dst)
+}
